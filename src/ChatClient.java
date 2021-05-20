04e9cc3d5083b4ec206bf430b2a284ddb35773cb
Wallace Patrick Lima Cardoso <zWallacez@users.noreply.github.com>
2021-05-20 19:28:47 -0300

    Output client
    O cliente é capaz de enviar mensagens para o servidor

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,17 +1,57 @@
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ChatClient {
 
+	public static final int PORT = 4000;//Porta para conexao
 	private static final String SERVER_ADDRESS = "127.0.0.1";
+	private Scanner sc;
+	private BufferedWriter output;
 	
+	public ChatClient() {
+		sc = new Scanner(System.in);
+	}
+	
+	//Conecta o cliente com o servidor
 	public void start() {
-		try(Socket clientSocket = new Socket(SERVER_ADDRESS,ChatServer.PORT)){
-			
+		try(Socket clientSocket = new Socket(SERVER_ADDRESS,PORT)){
+			/*
+			 * Stream outputStream para enviar uma mensagem para o server
+			 * A função write() envia apenas bytes, por isso o OutputStreamWriter
+			 * O OutputStreamWriter é uma stream de caracteres para stream de bytes.
+			 * O BufferedWriter cria um buffer para stream output
+			 * */
+			OutputStream outputStream = clientSocket.getOutputStream();
+			output = new BufferedWriter(new OutputStreamWriter(outputStream));
+		
+			try {
+				messageLoop();
+			}
+			catch(IOException e) {
+				e.getMessage();
+			}
 		}
 		catch(IOException b) {
 			System.out.println(b.getMessage());
 		}
+	}
+	
+	//Captura uma entrada do cliente
+	private void messageLoop() throws IOException {
+		
+		String msg;
+		do {
+			System.out.println("Enter yours message: ");
+			msg = sc.nextLine();
+			output.write(msg);
+			output.newLine();
+			output.flush();//Garante o envio da mensagem
+		}while(!msg.equalsIgnoreCase("exit"));
+
 	}
 	
 	public static void main(String[] args) {
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,13 +1,23 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
 
 	public static final int PORT = 4000;//Porta para conexao
+	private BufferedReader input;
+	private String msg;
 	
-	public void start() 
-	{//Implementa o socket do servidor
+	/*
+	 * Implementa o socket do servidor.
+	 * Se o objeto for realmente criado, significa que PORT (constante) estava 
+	 * fechada e foi aberta.
+	 * Obs.: Ocorre um erro se a porta já está em uso.
+	 * */
+	public void start() {
 		try(ServerSocket serverSocket = new ServerSocket(PORT)){
 			System.out.println("SUCCESS");
 			connectionLoop(serverSocket);
@@ -22,6 +32,15 @@ public class ChatServer {
 			try(Socket clientSocket = serverSocket.accept()){
 				System.out.println("Connection by: " +
 						clientSocket.getRemoteSocketAddress());
+				
+				/*
+				 * 
+				 * */
+				System.out.println(ipClient(clientSocket));
+				InputStream inputStream = clientSocket.getInputStream();
+				input = new BufferedReader(new InputStreamReader(inputStream));
+				msg = input.readLine();
+				System.out.println(ipClient(clientSocket) + " " + msg);
 			}
 			catch(IOException c) {
 				System.out.println("ERRO: " + c.getMessage());
@@ -30,10 +49,15 @@ public class ChatServer {
 		}
 	}
 	
+	public String ipClient(Socket clientSocket) {
+		return clientSocket.getInetAddress().getHostAddress();
+	}
+	
 	public static void main(String[] args) {
 		
 		ChatServer server = new ChatServer();
 		server.start();
+		
 		
 	}
 }
