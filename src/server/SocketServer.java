f3d56eb0820e397674b2f7f7e50d7d15da4d3d92
ianito <iaan.developer@gmail.com>
2021-06-05 19:18:10 -0300

    Added user list to chat

--- src/server/SocketServer.java
+++ src/server/SocketServer.java
@@ -6,75 +6,87 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.List;
+import java.util.Scanner;
 
-public class SocketServer{
+public class SocketServer {
 
 	private String ip;
-	
+
 	ServerSocket serverSocket;
-    Socket client;
-    Scanner sc;//Ler do console
-    List <PrintStream> outputClients;
+	Socket client;
+	Scanner sc;// Ler do console
+	List<PrintStream> outputClients;
+	ArrayList<String> userConnected = new ArrayList<>();;
 
-    public SocketServer() {
-    	
-        outputClients = new ArrayList<>();
-    }
+	public SocketServer() {
 
-    public void execute(ServerSocket server) throws IOException {
-    	
-    	System.out.println("Server iniciado");
-    	this.serverSocket = server;
-        while (true) {
+		outputClients = new ArrayList<>();
+	}
 
-            //Aceitando conexão cliente            
-            client = serverSocket.accept();
-            
-            ip = ipClient(client);
-            System.out.println("Connection by: " + ip);
+	public void execute(ServerSocket server) throws IOException {
 
-            //adicionar saida do cliente a lista de clientes
-            PrintStream ps = new PrintStream(client.getOutputStream());
-            outputClients.add(ps);
+		System.out.println("Server iniciado");
+		this.serverSocket = server;
+		while (true) {
 
-            //tratamento de clientes em uma nova Thread
-            TrataCliente thread = new TrataCliente(client.getInputStream(), this);
-            new Thread(thread).start();
+			// Aceitando conexão cliente
+			client = serverSocket.accept();
 
-        }
+			ip = ipClient(client);
+			System.out.println("Connection by: " + ip);
 
-    }
-    
-    public String ipClient(Socket client) {
-    	return client.getInetAddress().getHostAddress();
-    }
+			// adicionar saida do cliente a lista de clientes
+			PrintStream ps = new PrintStream(client.getOutputStream());
+			outputClients.add(ps);
 
-    public void distribuiMensagem(String msg) {
+			// tratamento de clientes em uma nova Thread
+			TrataCliente thread = new TrataCliente(client.getInputStream(), this, ps);
 
-        for (PrintStream cli : outputClients) {
-            cli.println(msg);
-            System.out.println(msg);
-        }
-    }
+			new Thread(thread).start();
 
-    private void secureClose(final Closeable resource) {
-        try {
-            if (resource != null) {
-                resource.close();
-            }
-        } catch (IOException ex) {
-            System.out.println("Erro = " + ex.getMessage());
-        }
+		}
 
-    }
+	}
 
-    public void encerrar() {
-        secureClose(serverSocket);
-        secureClose(client);
-        secureClose(sc);
-        System.exit(2);
-    }
-	
+	public String ipClient(Socket client) {
+		return client.getInetAddress().getHostAddress();
+	}
+
+	public void sendInfo(PrintStream output) {
+
+		for (PrintStream cli : outputClients) {
+			if (output == cli) {
+				output.println("[SERVER]WHO_ONLINE:"+userConnected.toString());
+			}
+		}
+
+	}
+
+	public void distribuiMensagem(String msg) {
+
+		for (PrintStream cli : outputClients) {
+			cli.println(msg);
+			System.out.println(msg);
+		}
+	}
+
+	private void secureClose(final Closeable resource) {
+		try {
+			if (resource != null) {
+				resource.close();
+			}
+		} catch (IOException ex) {
+			System.out.println("Erro = " + ex.getMessage());
+		}
+
+	}
+
+	public void encerrar() {
+		secureClose(serverSocket);
+		secureClose(client);
+		secureClose(sc);
+		System.exit(2);
+	}
+
 }
--- src/server/TrataCliente.java
+++ src/server/TrataCliente.java
@@ -1,26 +1,52 @@
 package server;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TrataCliente implements Runnable {
 
-    InputStream client;
-    SocketServer socketServer;
+	InputStream client;
+	SocketServer socketServer;
+	OutputStream outputStream;
+	PrintStream printStream;
 
-    public TrataCliente(InputStream client, SocketServer socketServer) {
-        this.client = client;
-        this.socketServer = socketServer;
-    }
-    
-    @Override
-    public void run() {
-        Scanner sc = new Scanner(this.client);
-        while(sc.hasNextLine()){
-            socketServer.distribuiMensagem(sc.nextLine());
-            //System.out.println(host+": "+scan.nextLine());
-        }
-        
-    }
+	public TrataCliente(InputStream client, SocketServer socketServer, PrintStream printStream) {
+		this.client = client;
+		this.socketServer = socketServer;
+		this.printStream = printStream;
+	}
+
+	@Override
+	public void run() {
+		Scanner sc = new Scanner(this.client);
+		while (sc.hasNextLine()) {
+			String msg = sc.nextLine();
+			System.out.println("SERVER: " + msg);
+			if (msg.contains("[SERVER]")) {
+				if(msg.equals("[SERVER]WHO_ONLINE")) {
+
+					socketServer.sendInfo(this.printStream);	
+				}else if(msg.contains("[SERVER]ADD_USER")) {
+					String splitted[] = msg.split(":");
+					socketServer.userConnected.add(new String(splitted[1]));
+					System.out.println("Adicionado: " + splitted[1]);
+				}else if(msg.contains("[SERVER]REMOVE_USER")) {
+					String splitted[] = msg.split(":");
+					socketServer.userConnected.remove(splitted[1]);
+				}
+				
+			
+
+				
+			} else {
+				socketServer.distribuiMensagem(msg);
+			}
+
+			// System.out.println(host+": "+scan.nextLine());
+		}
+
+	}
 
 }
